2064efcb0552ca8b112a6b235110d7d68e9b16a3
choman150 <choman150@amoeba.co.kr>
2025-08-19 11:48:29 +0900

    metatag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,25 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <title>JWT Encoder / Decoder</title>
+                <meta
+                    name="description"
+                    content="Online tool to encode and decode JSON Web Tokens (JWT). Secure, fast, and browser-based. JWT, JSON Web Token, JWT Encoder, JWT Decoder, JWT Online Tool, JWT Generator, JWT Debugger, jose, JWT HS256, decode JWT online, encode JWT token"
+                />
+                <meta name="keywords" content="JWT, JSON Web Token, JWT Encoder, JWT Decoder, JWT Online Tool, JWT Generator, JWT Debugger, jose, JWT HS256, decode JWT online, encode JWT token" />
+                <meta property="og:title" content="JWT Encoder / Decoder" />
+                <meta
+                    property="og:description"
+                    content="Encode and decode JWT tokens in your browser. Powered by jose and open-source. JWT, JSON Web Token, JWT Encoder, JWT Decoder, JWT Online Tool, JWT Generator, JWT Debugger, jose, JWT HS256, decode JWT online, encode JWT token"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://jwt.choman150.click/" />
+
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:locale:alternate" content="ko_KR" />
+                <meta name="author" content="choman150" />
+                <link rel="canonical" href="https://jwt.choman150.click" />
+                <link rel="icon" href="/favicon.ico" />
                 <meta name="monetag" content="aa6d902b00779634855e673864e8880a" />
                 <script
                     dangerouslySetInnerHTML={{
